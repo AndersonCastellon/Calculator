0042a52bfe55de661c0dd0cf9f3675eac9ef38ae
AndersonCastellon <anderson.ivan.fuentes@gmail.com>
2017-10-07 22:41:05 -0600

    Agregando metodos y constantes

---
+++ app/src/main/java/com/andersoncastellon/calculator/Constantes.java
@@ -0,0 +1,15 @@
+package com.andersoncastellon.calculator;
+
+
+
+public class Constantes {
+
+    public static final String OPERATOR_MULTI = "x";
+    public static final String OPERATOR_BETWEN = "÷";
+    public static final String OPERATOR_SUB = "-";
+    public static final String OPERATOR_SUM = "+";
+    public static final String OPERATOR_NULL = "null";
+    public static final String POINT = ".";
+
+    static final int DRAWABLE_RIGHT = 2;
+}
---
+++ app/src/main/java/com/andersoncastellon/calculator/OnResolveCallback.java
@@ -0,0 +1,10 @@
+package com.andersoncastellon.calculator;
+
+/**
+ * Created by AndersonCastellon on 7/10/2017.
+ */
+
+public interface OnResolveCallback {
+    void onShowMessage(int errorRes);
+    void onIsEditing();
+}
